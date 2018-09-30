d2c29c86b74024801fbe62b3b2db11e1783808dd
edumoritz <eduardo.moritz@hotmail.com>
2018-09-30 12:32:41 -0300

    criando query chats e listagem não funcionando ainda

--- src/app/chat/services/chat.service.ts
+++ src/app/chat/services/chat.service.ts
@@ -1,10 +1,30 @@
+import { AllChatsQuery, USER_CHATS_QUERY } from './chat.graphql';
 import { ChatModule } from '../chat.module';
 import { Injectable } from '@angular/core';
+import { Chat } from '../models/chat.model';
+import { Observable } from 'rxjs';
+import { Apollo } from 'apollo-angular';
+import { AuthService } from '../../core/services/auth.service';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: ChatModule
+  providedIn: 'root'
 })
 export class ChatService {
 
-  constructor() { }
+  constructor(
+    private apollo: Apollo,
+    private authService: AuthService
+  ) { }
+
+  getUserChats(): Observable<Chat[]> {
+    return this.apollo.query<AllChatsQuery>({
+      query: USER_CHATS_QUERY,
+      variables: {
+        userId: this.authService.authUser.id
+      }
+    }).pipe(
+      map(res => res.data.allChats)
+    )
+  }
 }
