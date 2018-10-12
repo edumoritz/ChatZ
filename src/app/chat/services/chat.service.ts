e76395daad3c1b8dbff06c6d535ab26ac5a62cd9
edumoritz <eduardo.moritz@hotmail.com>
2018-10-12 11:02:04 -0300

    Criando query dupla ou por usuario

--- src/app/chat/services/chat.service.ts
+++ src/app/chat/services/chat.service.ts
@@ -1,7 +1,7 @@
-import { ChatModule } from './../chat.module';
-import { AllChatsQuery, USER_CHATS_QUERY } from './chat.graphql';
+import { LoggedInUserQuery } from './../../core/services/auth.graphql';
+import { Chat } from './../models/chat.model';
+import { AllChatsQuery, USER_CHATS_QUERY, ChatQuery, CHAT_BY_ID_OR_BY_USERS_QUERY } from './chat.graphql';
 import { Injectable } from '@angular/core';
-import { Chat } from '../models/chat.model';
 import { Observable } from 'rxjs';
 import { Apollo } from 'apollo-angular';
 import { AuthService } from '../../core/services/auth.service';
@@ -25,6 +25,22 @@ export class ChatService {
       }
     }).pipe(
       map(res => res.data.allChats)
+    )
+  }
+
+  getChatByIdOrByUsers(chatOrUserId: string): Observable<Chat> {
+    return this.apollo.query<ChatQuery | AllChatsQuery>({
+      query: CHAT_BY_ID_OR_BY_USERS_QUERY,
+      variables: {
+        chatId: chatOrUserId,
+        loggedUserId: this.authService.authUser.id,
+        targetUserId: chatOrUserId
+      }
+    }).pipe(
+      map(res => {
+        const chat: Chat = (res.data['Chat']) ? res.data['Chat'] : res.data['allChats'][0];
+        return chat;
+      })
     )
   }
 }
