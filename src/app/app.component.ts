bf532a44b6bed2230702747c3a17d92e8a411d47
edumoritz <eduardo.moritz@hotmail.com>
2018-11-02 11:57:29 -0300

    entendendo base component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,21 +4,39 @@ import { ErrorService } from './core/services/error.service';
 import { take } from 'rxjs/operators';
 import { AuthService } from './core/services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { BaseComponent } from './shared/components/base.component';
 
 @Component({
   selector: 'app-root',
   template: `
+    <ul>
+      <li *ngFor="let c of collection; trackBy: trackByFn">Item {{ c.id }}</li>
+    </ul>
+    <button (click)="collection.push({ id: collection.length + 1 })">Add</button>
+    <button (click)="collection.shift()">Remove</button>
+    <button (click)="onRefresh()">Refresh</button>
     <router-outlet></router-outlet>
   `
 })
-export class AppComponent implements OnInit{
+export class AppComponent extends BaseComponent<any> implements OnInit{
+
+
+  collection = [
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 }
+  ]
 
   constructor(
     private appConfig: AppConfigService,
     private authService: AuthService,
     private errorService: ErrorService,
     private snackBar: MatSnackBar
-  ) {}
+  ) {
+    super();
+  }
 
   ngOnInit(): void {
     this.authService.autoLogin()
@@ -34,6 +52,18 @@ export class AppComponent implements OnInit{
             );
           }
         );
+  }
+
+  onRefresh(): void {
+    this.collection = [
+      { id: 1 },
+      { id: 2 },
+      { id: 3 },
+      { id: 4 },
+      { id: 5 },
+      { id: 6 },
+      { id: 7 }
+    ]
   }
 
 }
---
+++ src/app/shared/components/base.component.ts
@@ -0,0 +1,5 @@
+export class BaseComponent<T extends {id: number | string}> {
+  trackByFn(index: number, item: T): number | string {
+    return item.id;
+  }
+}
