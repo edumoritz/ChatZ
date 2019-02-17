c16f495e2e69735d47ad421ff4e08543f655c98d
edumoritz <eduardo.moritz@hotmail.com>
2019-02-17 12:02:19 -0300

    utilizando FormArray

--- src/app/chat/components/chat-add-group/chat-add-group.component.ts
+++ src/app/chat/components/chat-add-group/chat-add-group.component.ts
@@ -1,5 +1,9 @@
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl, FormArray } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { User } from './../../../core/models/user.model';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-chat-add-group',
@@ -9,22 +13,31 @@ import { Component, OnInit } from '@angular/core';
 export class ChatAddGroupComponent implements OnInit {
 
   newGroupForm: FormGroup;
+  users$: Observable<User[]>
 
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private userService: UserService
   ) { }
 
   ngOnInit(): void {
+    this.users$ = this.userService.users$;
     this.createForm();
   }
 
   private createForm(): void {
     this.newGroupForm = this.fb.group({
-      title: ['', [Validators.required, Validators.minLength(3)]]
+      title: this.fb.control('', [Validators.required, Validators.minLength(3)]),
+      members: this.fb.array([], Validators.required)
     })
   }
 
   get title(): FormControl { return <FormControl>this.newGroupForm.get('title'); }
+  get members(): FormArray { return <FormArray>this.newGroupForm.get('members'); }
+
+  addMember(user: User): void {
+    this.members.push(this.fb.group(user));
+  }
 
   onSubmit() {
     console.log(this.newGroupForm.value)
